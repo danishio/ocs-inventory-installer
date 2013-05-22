a43e8e947615cad9c90f3448622427b337c68a02
danishio <daniel.nishio@gmail.com>
2013-05-22 16:44:02 -0300

    Update OCS.sh

--- OCS.sh
+++ OCS.sh
@@ -1,12 +1,12 @@
 apt-get install dmidecode libxml-simple-perl libio-compress-perl libnet-ip-perl libwww-perl libdigest-md5-perl libnet-ssleay-perl libnet-snmp-perl libproc-pid-file-perl libproc-daemon-perl net-tools libsys-syslog-perl pciutils smartmontools read-edid nmap
 
-wget https://launchpad.net/ocsinventory-unix-agent/stable-2.0/2.0.5/+download/Ocsinventory-Unix-Agent-2.0.5.tar.gz
+wget https://launchpad.net/ocsinventory-unix-agent/stable-2.1/2.1rc1/+download/Ocsinventory-Unix-Agent-2.1rc1.tar.gz
 
 mkdir /bin/OCS
-mv Ocsinventory-Unix-Agent-2.0.5.tar.gz /bin/OCS/Ocsinventory-2.0.5.tar.gz
+mv Ocsinventory-Unix-Agent-2.1rc1.tar.gz /bin/OCS/Ocsinventory-2.1rc1.tar.gz
 cd /bin/OCS
-tar xzf Ocsinventory-2.0.5.tar.gz
-cd Ocsinventory-Unix-Agent-2.0.5
+tar xzf Ocsinventory-2.1rc1.tar.gz
+cd Ocsinventory-2.1rc1.tar.gz
 perl Makefile.PL
 make
 make install
